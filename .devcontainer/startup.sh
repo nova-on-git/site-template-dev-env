5414c2ae8db2a2404982aea2c551b95ba373b2bf
Cody Wakeford <codypwakeford@gmail.com>
2024-10-11 23:22:17 +0100

    added extensions, fixed ts errors

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-echo "[Veloris] Installing dependencies and spinning up a dev server..."
+
+echo ""
+echo -e "\e[32m[Veloris]\e[0m Installing dependencies and spinning up a dev server..."
+echo ""
+
 
 cd /app/veloris-dashboard
+
+echo -e "\e[32m[Veloris]\e[0m Pulling veloris-dashboard"
 git pull
 pnpm install
+nuxt build
 
 cd /app/veloris-site-template
+
+echo -e "\e[32m[Veloris]\e[0m Pulling veloris-site-template"
 git pull
 pnpm install
 
