de247a62b1a8536e10dcc5a951b35c1ff873e5f9
Cody Wakeford <codypwakeford@gmail.com>
2024-10-12 09:32:20 +0100

    final changes

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 
 echo ""
-echo -e "\e[32m[Veloris]\e[0m Installing dependencies and spinning up a dev server..."
-echo ""
-
+echo -e "\e[32m[Veloris] Installing dependencies and spinning up a dev server... \e[0m"
 
 cd /app/veloris-dashboard
 
-echo -e "\e[32m[Veloris]\e[0m Pulling veloris-dashboard"
+echo ""
+echo ""
+echo -e "\e[32m[Veloris] Pulling latest veloris-dashboard from git. \e[0m"
 git pull
+
+echo ""
+echo ""
+echo -e "\e[32m[Veloris] Installing veloris-dashboard dependencies. \e[0m"
 pnpm install
+
+echo ""
+echo ""
+echo -e "\e[32m[Veloris] Building veloris-dashboard for .nuxt cache. \e[0m"
 nuxt build
 
 cd /app/veloris-site-template
 
-echo -e "\e[32m[Veloris]\e[0m Pulling veloris-site-template"
+echo ""
+echo ""
+echo -e "\e[32m[Veloris] Pulling latest veloris-site-template from git. \e[0m"
 git pull
+
+echo ""
+echo ""
+echo -e "\e[32m[Veloris] Installing veloris-site-template dependencies. \e[0m"
 pnpm install
 
+echo ""
+echo ""
+echo -e "\e[32m[Veloris] Starting development server... \e[0m"
 nuxt dev --no-fork
